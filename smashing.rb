b1421231c1bd9c752faaaa728448da4c9f9fd428
Eugene Shved <snuggleshved@gmail.com>
2024-07-18 15:40:04 +0200

    Add execution for ImageAnalyzer

--- smashing.rb
+++ smashing.rb
@@ -8,6 +8,7 @@ class SmashingCLI
       arguments = ArgumentParser.new.parse
       link = LinkFinder.new(arguments[:month], arguments[:year]).find
       image_links = ImageLinkFinder.new(link).find
+      verified_image_links = ImageAnalyzer.new(image_links, arguments[:theme]).analyze
     rescue CliArgumentError => e
       puts e.message
     end
