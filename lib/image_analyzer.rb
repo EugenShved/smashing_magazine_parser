b7c38b3020ca2058607c049aa493ca7f5b49f610
Eugene Shved <snuggleshved@gmail.com>
2024-07-18 15:34:29 +0200

    Add ImageAnalyzer

---
+++ lib/image_analyzer.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require "openai"
+require "open-uri"
+
+class ImageAnalyzer
+  API_KEY = "CHAT GPT API KEY"
+  attr_reader :urls, :theme
+
+  def initialize(urls, theme)
+    @urls = urls
+    @theme = theme
+  end
+
+  def analyze
+    url_analytics = []
+    urls.each do |url|
+      verify_status = verify_image_by_theme(url)
+      url_analytics << url if verify_status
+    end
+    url_analytics
+  end
+
+  private
+
+  def verify_image_by_theme(url)
+    content = "Check the image #{url} and say me in one number 1 or 0 does it similar for theme - #{theme}"
+    response = chat_gpt_client.chat(
+      parameters: {
+        model: "gpt-4o",
+        messages: [{ role: "user", content: content }],
+        temperature: 0.2,
+      },
+    )
+    response["choices"][0]["message"]["content"].to_i == 1
+  end
+
+  def chat_gpt_client
+    @chat_gpt_client ||= OpenAI::Client.new(api_key: API_KEY)
+  end
+end
